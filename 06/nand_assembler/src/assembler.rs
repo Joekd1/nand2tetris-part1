963949d6abd70c5f89078284070f2bfd754a3cf1
joe <joenikushimi@protonmail.com>
2025-10-17 17:27:46 +0100

    feat(assembler): handle command line arguments

---
+++ 06/nand_assembler/src/assembler.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::path::Path;
+
+pub struct Assembler;
+
+impl Assembler {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, Box<dyn Error>> {
+        // The first value is the name of the program
+        args.next();
+
+        let file_path = args.next().ok_or("Didn't get file path").and_then(|path| {
+            Path::new(&path)
+                .is_file()
+                .then_some(Path::new(&path))
+                .ok_or("File doesn't exist")?
+                .extension()
+                .filter(|&ext| ext == "asm")
+                .map(|_| path.clone())
+                .ok_or("Wrong file extension")
+        })?;
+
+        Ok(Assembler)
+    }
+}
---
+++ 06/nand_assembler/src/main.rs
@@ -0,0 +1,12 @@
+use assembler::Assembler;
+mod assembler;
+
+use std::env;
+use std::process;
+
+fn main() {
+    let mut assembler = Assembler::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(1);
+    });
+}
