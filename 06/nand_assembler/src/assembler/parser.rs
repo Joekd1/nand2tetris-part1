ac6fb164be4cde5cf56b748ed6351397f22f161f
joe <joenikushimi@protonmail.com>
2025-10-18 14:36:48 +0100

    feat(assembler): implement has_more_lines

--- 06/nand_assembler/src/assembler/parser.rs
+++ 06/nand_assembler/src/assembler/parser.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 pub struct Parser {
     pub contents: Vec<String>,
+    pub file_index: i32, 
 }
 
 
@@ -15,8 +16,57 @@ impl Parser {
             .map(|s| s.trim().to_string())
             .collect();
 
+	let file_index = -1;
+
         Ok(Self {
             contents,
+	    file_index
         })
     }
+
+    pub fn has_more_lines(&self) -> bool {
+        // checks if there are still lines
+        self.contents.len() as i32 > (self.file_index + 1)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    struct TestContext {
+        empty: Parser,
+    }
+
+    impl Drop for TestContext {
+        fn drop(&mut self) {
+            println!("Test teardown ...");
+        }
+    }
+
+    fn setup() -> TestContext {
+        println!("Test setup ...");
+
+        let contents = Vec::new();
+        let file_index = -1;
+
+        TestContext {
+            empty: Parser {
+                contents,
+                file_index,
+            }
+	}
+    }
+
+    #[test]
+    fn has_more_lines_of_empty_vec_is_false() {
+        // Arrange
+        let ctx = setup();
+
+        // Assert
+        assert_eq!(ctx.empty.has_more_lines(), false);
+    }
+
 }
